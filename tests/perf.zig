edc1b5a53417b600d85916e50cd2e90a9bd80e95
Quentin <49767738+quentin-k@users.noreply.github.com>
2022-10-15 17:45:32 -0500

    Updated perf.zig

--- tests/perf.zig
+++ tests/perf.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const hc256 = @import("hc256");
+const c_clockcycle = @intToFloat(f64, std.time.ns_per_us);
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
@@ -13,48 +14,41 @@ pub fn main() !void {
     var key: [32]u8 = undefined;
     var iv: [32]u8 = undefined;
 
-    var page_4k = try allocator.alloc(u8, 4 * 1024);
-    defer allocator.free(page_4k);
-    var page_8k = try allocator.alloc(u8, 8 * 1024);
-    defer allocator.free(page_8k);
-    var page_16k = try allocator.alloc(u8, 16 * 1024);
-    defer allocator.free(page_16k);
-    var page_32k = try allocator.alloc(u8, 32 * 1024);
-    defer allocator.free(page_32k);
-
-    try std.os.getrandom(&key);
-    try std.os.getrandom(&iv);
-
-    ts_begin = std.time.nanoTimestamp();
-    var c4k = hc256.Hc256.init(key, iv);
-    c4k.applyStream(page_4k);
-    ts_end = std.time.nanoTimestamp();
-    try stdout.print("It took {}ns to apply the stream on a 4 kilobyte page\n", .{ts_end - ts_begin});
-
-    try std.os.getrandom(&key);
-    try std.os.getrandom(&iv);
-
-    ts_begin = std.time.nanoTimestamp();
-    var c8k = hc256.Hc256.init(key, iv);
-    c8k.applyStream(page_8k);
-    ts_end = std.time.nanoTimestamp();
-    try stdout.print("It took {}ns to apply the stream on a 8 kilobyte page\n", .{ts_end - ts_begin});
-
-    try std.os.getrandom(&key);
-    try std.os.getrandom(&iv);
-
-    ts_begin = std.time.nanoTimestamp();
-    var c16k = hc256.Hc256.init(key, iv);
-    c16k.applyStream(page_16k);
-    ts_end = std.time.nanoTimestamp();
-    try stdout.print("It took {}ns to apply the stream on a 16 kilobyte page\n", .{ts_end - ts_begin});
+    var page_32mb = try allocator.alloc(u8, 32 * 1024 * 1024);
+    defer allocator.free(page_32mb);
 
     try std.os.getrandom(&key);
     try std.os.getrandom(&iv);
 
     ts_begin = std.time.nanoTimestamp();
     var c32k = hc256.Hc256.init(key, iv);
-    c32k.applyStream(page_32k);
     ts_end = std.time.nanoTimestamp();
-    try stdout.print("It took {}ns to apply the stream on a 32 kilobyte page\n", .{ts_end - ts_begin});
+
+    const init_time = ts_end - ts_begin;
+    try stdout.print(
+        \\Initialization time:
+        \\{}ns
+        \\{d} clock cycles
+        \\
+    , .{ init_time, clockCycles(init_time, 1) });
+
+    ts_begin = std.time.nanoTimestamp();
+    c32k.applyStream(page_32mb);
+    ts_end = std.time.nanoTimestamp();
+
+    const cycles_per_byte = clockCycles(ts_end - ts_begin, page_32mb.len);
+    try stdout.print(
+        \\It took {}ns to apply the stream on a 32 megabyte page
+        \\Cycles per byte: {d:>.3}
+        \\Cycles per bit: {d:>.3}
+        \\
+    , .{ ts_end - ts_begin, cycles_per_byte, cycles_per_byte * 8.0 });
+}
+
+fn clockCycles(nanoseconds: i128, bytes: usize) f64 {
+    const ns_f64 = @intToFloat(f64, nanoseconds);
+    const clocks = ns_f64 / c_clockcycle;
+    const b_f64 = @intToFloat(f64, bytes);
+    const clocks_per_byte = clocks / b_f64;
+    return clocks_per_byte;
 }
